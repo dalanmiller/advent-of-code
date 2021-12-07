4107fef01c64c47d2e6379d0d34352023a7c2fa4
dalan <863286+dalanmiller@users.noreply.github.com>
2021-12-07 21:51:32 +1100

    Day 7

---
+++ 2021/07/07.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
+func parseInput(input string) []int {
+	crabs := strings.Split(input, ",")
+
+	crabs_values := make([]int, len(crabs))
+	var max int
+	for i, v := range crabs {
+		n, _ := strconv.Atoi(v)
+		crabs_values[i] = n
+
+		if n > max {
+			max = n
+		}
+	}
+
+	field := make([]int, max+1)
+	for _, cv := range crabs_values {
+		field[cv]++
+	}
+
+	return field
+}
+
+func calculateFuelRequirement(distance int, numCrabs int, memo map[int]int) int {
+	if val, ok := memo[distance]; ok {
+		return val * numCrabs
+	}
+
+	v := 0
+	for i := 1; i <= distance; i++ {
+		v += i
+	}
+
+	memo[distance] = v
+	return v * numCrabs
+}
+
+func run(input string) (int, int) {
+	field := parseInput(input)
+
+	// possible_positions
+	part_one_fuel_required := make([]int, len(field))
+	part_two_fuel_required := make([]int, len(field))
+	memo := make(map[int]int)
+	for i := range field {
+		sum_one, sum_two := 0, 0
+
+		for j := range field {
+
+			// Continue given the crabs in that location don't need to go anywhere
+			if i == j {
+				continue
+			}
+
+			distance := int(math.Abs(float64(i - j)))
+			// Distance between target position and current position
+			//  times number of crab ships
+
+			// Part 1 calculation
+			sum_one += distance * field[j]
+
+			// Part 2 calculation
+			// TIL maps are a reference value in Golang and don't need
+			//  to have a pointer.
+			sum_two += calculateFuelRequirement(distance, field[j], memo)
+
+		}
+
+		part_one_fuel_required[i] = sum_one
+		part_two_fuel_required[i] = sum_two
+	}
+
+	min_part_one_fuel_cost := math.MaxInt
+	min_part_two_fuel_cost := math.MaxInt
+	for i := range part_one_fuel_required {
+		if part_one_fuel_required[i] < min_part_one_fuel_cost {
+			min_part_one_fuel_cost = part_one_fuel_required[i]
+		}
+
+		if part_two_fuel_required[i] < min_part_two_fuel_cost {
+			min_part_two_fuel_cost = part_two_fuel_required[i]
+		}
+	}
+	return min_part_one_fuel_cost, min_part_two_fuel_cost
+}
---
+++ 2021/07/07_test.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func TestExamplesSevenOne(t *testing.T) {
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{"16,1,2,0,4,2,7,1,2,14", 37},
+	}
+
+	for _, test := range tests {
+		result, _ := run(test.test)
+		if test.expected != result {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func TestSevenOne(t *testing.T) {
+	file, err := os.ReadFile("./input")
+	if err != nil {
+		log.Fatalf("could not read file")
+	}
+
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{string(file), 348996},
+	}
+
+	for _, test := range tests {
+		result, _ := run(test.test)
+		if result != test.expected {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func TestFuelDistanceCalculator(t *testing.T) {
+	tests := []struct {
+		distance int
+		expected int
+	}{
+		{4, 10},
+		{1, 1},
+		{2, 3},
+		{3, 6},
+		{5, 15},
+	}
+
+	memo := make(map[int]int)
+	for _, test := range tests {
+		result := calculateFuelRequirement(test.distance, 1, memo)
+		if result != test.expected {
+			log.Fatalf("Distance calculation failed, got %d, expected %d", result, test.expected)
+		}
+	}
+}
+func TestExamplesSevenTwo(t *testing.T) {
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{"16,1,2,0,4,2,7,1,2,14", 168},
+	}
+
+	for _, test := range tests {
+		_, result := run(test.test)
+		if test.expected != result {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func TestSevenTwo(t *testing.T) {
+	file, err := os.ReadFile("./input")
+	if err != nil {
+		log.Fatalf("could not read file")
+	}
+
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{string(file), 98231647},
+	}
+
+	for _, test := range tests {
+		_, result := run(test.test)
+		if result != test.expected {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func BenchmarkRunWithMin(b *testing.B) {
+	file, _ := os.ReadFile("./input")
+
+	for i := 0; i < b.N; i++ {
+		_, result := run(string(file))
+		result++
+	}
+}
