bcdf461411a19cdb35db4c2a9578a3537c9a91df
dalan <863286+dalanmiller@users.noreply.github.com>
2022-12-02 21:55:56 +1100

    2022 Day 2

---
+++ 2022/02/02.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"strings"
+)
+
+type shape string
+
+const (
+	R1 string = "A"
+	P1        = "B"
+	S1        = "C"
+	R2        = "X"
+	P2        = "Y"
+	S2        = "Z"
+)
+
+var pointMap = map[string]int{
+	"A": 1,
+	"B": 2,
+	"C": 3,
+}
+
+func readInputOne(input string) int {
+	lines := strings.Split(input, "\n")
+
+	score := 0
+	for _, line := range lines {
+		vals := strings.Split(line, " ")
+		o := vals[0]
+		m := vals[1]
+
+		switch o {
+		case "A":
+			if m == "Y" { // win
+				score += 8
+			} else if m == "Z" { // lose
+				score += 3
+			} else {
+				score += 4 // draw
+			}
+		case "B":
+			if m == "Z" { //win
+				score += 9
+			} else if m == "X" { // lose
+				score += 1
+			} else { // draw
+				score += 5
+			}
+		case "C":
+			if m == "X" {
+				score += 7
+			} else if m == "Y" {
+				score += 2
+			} else {
+				score += 6
+			}
+		}
+	}
+
+	return score
+}
+
+func readInputTwo(input string) int {
+	lines := strings.Split(input, "\n")
+
+	// X == lose
+	// y == draw
+	// z == win
+
+	score := 0
+	for _, line := range lines {
+		vals := strings.Split(line, " ")
+		o := vals[0]
+		m := vals[1]
+
+		switch o {
+		case "A":
+			switch m {
+			case "X":
+				score += 3
+			case "Y":
+				score += 4
+			case "Z":
+				score += 8
+			}
+		case "B":
+			switch m {
+			case "X":
+				score += 1
+			case "Y":
+				score += 5
+			case "Z":
+				score += 9
+			}
+		case "C":
+			switch m {
+			case "X":
+				score += 2
+			case "Y":
+				score += 6
+			case "Z":
+				score += 7
+			}
+		}
+	}
+
+	return score
+}
+
+func run(input string) (int, int) {
+	scoreOne := readInputOne(input)
+	scoreTwo := readInputTwo(input)
+
+	return scoreOne, scoreTwo
+
+}
---
+++ 2022/02/02_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+const EXAMPLE = `A Y
+B X
+C Z`
+
+func TestExamplesOneOne(t *testing.T) {
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{EXAMPLE, 15},
+	}
+
+	for _, test := range tests {
+		result, _ := run(test.test)
+		if test.expected != result {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func TestOneOne(t *testing.T) {
+	file, err := os.ReadFile("./input")
+	if err != nil {
+		log.Fatalf("could not read file")
+	}
+
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{string(file), 13565},
+	}
+
+	for _, test := range tests {
+		result, _ := run(test.test)
+		if result != test.expected {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func TestExamplesOneTwo(t *testing.T) {
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		{EXAMPLE, 12},
+	}
+
+	for _, test := range tests {
+		_, result := run(test.test)
+		if test.expected != result {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
+
+func TestOneTwo(t *testing.T) {
+	file, err := os.ReadFile("./input")
+	if err != nil {
+		log.Fatalf("could not read file")
+	}
+
+	tests := []struct {
+		test     string
+		expected int
+	}{
+		// 12334 too low
+		{string(file), 12424},
+	}
+
+	for _, test := range tests {
+		_, result := run(test.test)
+		if result != test.expected {
+			t.Fatalf("Result % d != expected % d", result, test.expected)
+		}
+	}
+}
