bdc551bbdb7a5b84c469911ccc241924c1f48264
dalan <863286+dalanmiller@users.noreply.github.com>
2022-12-24 14:32:18 +1100

    2022 Day 13 - refactor

--- 2022/13/13.go
+++ 2022/13/13.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"io"
-	"strconv"
+	"log"
 	"strings"
-	"unicode"
 )
 
 type Outcome int
@@ -16,26 +15,7 @@ const (
 	GREATER
 )
 
-type DataType string
-
-const (
-	LIST    DataType = "LIST"
-	INTEGER DataType = "INTEGER"
-)
-
-type pair struct {
-	Left  *packetData
-	Right *packetData
-	// Raw   string
-}
-
-type packetData struct {
-	DataType DataType
-	List     []*packetData
-	Integer  int
-}
-
-func (p pair) Compare() (order bool) {
+func compare(left, right []interface{}) Outcome {
 
 	// 1. If both values are integers, the lowest one should come first. If otherwise, the values
 	// . are not in the right order. If same, continue.
@@ -46,170 +26,77 @@ func (p pair) Compare() (order bool) {
 	// 3. If exactly one value is an integer, convert the integer to a list which contains
 	// . that integer as its only value then retry the comparison.
 
-	var left, right *packetData
-	// log.Println(p.Raw)
+	// https://github.com/alexchao26/advent-of-code-go/blob/main/2022/day13/main.go#L103
+	// Had no idea you could effectively abuse interface in golang in this way
+	// Basically parse using json.Unmarshal then hope for the best when casting?
 
-	for i := 0; i < len(p.Left.List); i++ {
-
-		if i > len(p.Right.List)-1 {
-			return false
+	for i := 0; i < len(left); i++ {
+		if i > len(right)-1 {
+			return GREATER
 		}
 
-		left = p.Left.List[i]
-		right = p.Right.List[i]
-		switch {
+		// Have to use float64 and not int as this is what the json Unmarshal uses
+		leftN, isLeftInt := left[i].(float64)
+		rightN, isRightInt := right[i].(float64)
 
-		case left.DataType == INTEGER && right.DataType == INTEGER:
-			switch {
-			case left.Integer < right.Integer:
-				return true
-			case left.Integer == right.Integer:
-				continue
-			case left.Integer > right.Integer:
-				return false
-			}
+		leftList, _ := left[i].([]interface{})
+		rightList, _ := left[i].([]interface{})
 
-		case left.DataType == LIST && right.DataType == LIST:
-			switch compareLists(left, right) {
-			case LESS:
-				return true
-			case EQUAL:
-				continue
-			case GREATER:
-				return false
-			}
-
-		case (left.DataType == LIST && right.DataType == INTEGER) || (left.DataType == INTEGER && right.DataType == LIST):
-			if left.DataType == INTEGER {
-				left = &packetData{
-					DataType: LIST,
-					List:     []*packetData{{DataType: INTEGER, Integer: left.Integer}},
+		if isLeftInt && isRightInt {
+			log.Println(leftN, rightN)
+			if leftN != rightN {
+				switch leftN < rightN {
+				case true:
+					return LESS
+				case false:
+					return GREATER
 				}
+			}
+		} else if isLeftInt || isRightInt {
+			if isLeftInt {
+				leftList = []interface{}{leftN}
 			} else {
-				right = &packetData{
-					DataType: LIST,
-					List:     []*packetData{{DataType: INTEGER, Integer: right.Integer}},
-				}
+				rightList = []interface{}{rightN}
 			}
+		}
+		outcome := compare(leftList, rightList)
+		switch outcome {
+		case LESS, GREATER:
+			return outcome
+		case EQUAL:
+			continue
 
-			switch compareLists(left, right) {
-			case LESS:
-				return true
-			case EQUAL:
-				continue
-			case GREATER:
-				return false
-			}
 		}
 	}
 
-	if len(p.Left.List) < len(p.Right.List) {
-		return true
-	} else if len(p.Left.List) > len(p.Right.List) {
-		return false
-	}
-
-	return order
-}
-
-func compareLists(a *packetData, b *packetData) Outcome {
-	left, right := *a, *b
-
-	for i := range left.List {
-		if i > len(left.List)-1 {
-			return LESS
-		}
-
-		if i > len(right.List)-1 {
-			return GREATER
-		}
-
-		// log.Println(left.List[i], right.List[i])
-
-		if left.List[i].DataType == LIST && right.List[i].DataType == LIST {
-			switch compareLists(left.List[i], right.List[i]) {
-			case LESS:
-				return LESS
-			case EQUAL:
-				continue
-			case GREATER:
-				return GREATER
-			}
-		}
-
-		if left.List[i].Integer < right.List[i].Integer {
-			return LESS
-		}
-		if left.List[i].Integer > right.List[i].Integer {
-			return GREATER
-		}
+	if len(left) < len(right) {
+		return LESS
 	}
 
 	return EQUAL
 }
 
-func readInput(input io.Reader) (pairs []pair) {
+func readInput(input io.Reader) (pairs [][2][]interface{}) {
 
-	scanner := bufio.NewScanner(input)
-	for scanner.Scan() {
-		first := scanner.Text()
-		scanner.Scan()
-		second := scanner.Text()
+	b, _ := io.ReadAll(input)
 
-		pair := pair{
-			Left:  parsePacketString(first),
-			Right: parsePacketString(second),
-			// Raw:   fmt.Sprintf("%s | %s", first, second),
-		}
-		pairs = append(pairs, pair)
-
-		// Jump blank line
-		scanner.Scan()
+	for _, packetPair := range strings.Split(string(b), "\n\n") {
+		packets := strings.Split(packetPair, "\n")
+		pairs = append(pairs, [2][]interface{}{
+			parsePacketString(packets[0]),
+			parsePacketString(packets[1]),
+		})
 	}
 
 	return pairs
 }
 
-func parsePacketString(p string) *packetData {
+func parsePacketString(p string) []interface{} {
 	// [1,[2,[3,[4,[5,6,7]]]],8,9]
 
-	s := bufio.NewScanner(strings.NewReader(strings.TrimLeft(p, " ")))
-	s.Split(bufio.ScanRunes)
-
-	var current *packetData
-	depth := []*packetData{}
-
-	// first = current
-	for s.Scan() {
-		r := []rune(s.Text())[0]
-		switch {
-		case r == '[':
-			current = &packetData{
-				DataType: LIST,
-				List:     []*packetData{},
-			}
-
-			// Make sure that we update the last layer
-			if len(depth) > 0 {
-				depth[len(depth)-1].List = append(depth[len(depth)-1].List, current)
-			}
-
-			depth = append(depth, current)
-		case r == ']':
-			if len(depth) > 1 {
-				depth = depth[:len(depth)-1]
-			}
-			current = depth[len(depth)-1]
-		case unicode.IsDigit(r):
-			v, _ := strconv.Atoi(string(r))
-			current.List = append(current.List, &packetData{
-				DataType: INTEGER,
-				Integer:  v,
-			})
-		}
-	}
-
-	return depth[0]
+	packet := []interface{}{}
+	json.Unmarshal([]byte(p), &packet)
+	return packet
 }
 
 func run(input io.Reader) int {
@@ -217,7 +104,8 @@ func run(input io.Reader) int {
 
 	s := 0
 	for i, p := range pairs {
-		if p.Compare() {
+		log.Println(p[0], p[1])
+		if compare(p[0], p[1]) == LESS {
 			s += i + 1
 		}
 	}
--- 2022/13/13_test.go
+++ 2022/13/13_test.go
@@ -6,45 +6,7 @@ import (
 	"os"
 	"strings"
 	"testing"
-
-	"github.com/google/go-cmp/cmp"
 )
-
-var EXAMPLES = []struct {
-	stringPair string
-	outcome    bool
-}{
-	{`[1,1,3,1,1]
-[1,1,5,1,1]`, true},
-	{`[[1],[2,3,4]]
-[[1],4]`, true},
-	{`[9]
-[[8,7,6]]`, false},
-	{`[[4,4],4,4]
-[[4,4],4,4,4]`, true},
-	{`[7,7,7,7]
-[7,7,7]`, false},
-	{`[]
-[3]`, true},
-	{`[[[]]]
-[[]]`, false},
-	{`[1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]`, false}}
-
-func TestExamplesThirteenOne(t *testing.T) {
-	for i, example := range EXAMPLES {
-		split := strings.Split(example.stringPair, "\n")
-		pair := pair{
-			Left:  parsePacketString(split[0]),
-			Right: parsePacketString(split[1]),
-			// Raw:   fmt.Sprintf("%s | %s", split[0], split[1]),
-		}
-		result := pair.Compare()
-		if result != example.outcome {
-			log.Fatalf("%d - Result %v != expected %v", i, result, example.outcome)
-		}
-	}
-}
 
 func TestExampleThirteen(t *testing.T) {
 	test := `[1,1,3,1,1]
@@ -78,42 +40,6 @@ func TestExampleThirteen(t *testing.T) {
 
 }
 
-func TestParsePacketString(t *testing.T) {
-	tests := []struct {
-		test     string
-		expected *packetData
-	}{
-		{"[]", &packetData{
-			DataType: LIST,
-			List:     []*packetData{},
-		}},
-		{"[3]", &packetData{
-			DataType: LIST,
-			List:     []*packetData{{DataType: INTEGER, Integer: 3}},
-		}},
-
-		{"[[[]]]", &packetData{
-			DataType: LIST,
-			List: []*packetData{
-				{DataType: LIST, List: []*packetData{{DataType: LIST, List: []*packetData{}}}}},
-		}},
-		{"[[]]", &packetData{
-			DataType: LIST,
-			List: []*packetData{
-				{DataType: LIST, List: []*packetData{}},
-			},
-		}},
-	}
-
-	for i, test := range tests {
-		result := parsePacketString(test.test)
-		if !cmp.Equal(result, test.expected) {
-			t.Fatalf("%d | Result %v != expected %v", i, result, test.expected)
-		}
-	}
-
-}
-
 func TestThirteenOne(t *testing.T) {
 	file, _ := os.Open("./input")
 	defer file.Close()
@@ -124,6 +50,8 @@ func TestThirteenOne(t *testing.T) {
 		expected int
 	}{
 		// 3650 too low
+		// 4442 too low
+		// 6251 too high
 		{reader, 0},
 	}
 
